aecf1dd19d05a13e95e49548feac911bad4645d5
ArturFromMaurus <97187315+ArturFromMaurus@users.noreply.github.com>
2023-03-17 03:48:29 +0200

    window.onload

--- yksliikmed_hulkliikmed_ja_tehted_nendega/Sulgude_avamine/Sulgude_avamine.js
+++ yksliikmed_hulkliikmed_ja_tehted_nendega/Sulgude_avamine/Sulgude_avamine.js
@@ -1,4 +1,3 @@
-
 var step=5;
 let X=0;
 let Y=0;
@@ -39,10 +38,12 @@ var answerMathField = MQ.MathField(answerSpan, {
 
 
 // ----------------------------------------- HTML ToolTip -------------------------------------------
-
-var tooltip = document.createElement("div");
-tooltip.style.backgroundColor = "rgba(255, 255, 255, 255)";
-tooltip.style.color = "black";
+window.onload = function() {
+  
+tooltip = document.createElement("div");
+tooltip.style.backgroundColor = "rgba(9,9,96,0.95)"
+tooltip.style.color = "white";
+tooltip.style.borderRadius="25px";
 tooltip.style.padding = "10px";
 tooltip.style.position = "absolute";
 tooltip.style.display = "none";
@@ -51,23 +52,25 @@ tooltip.style.border="solid 2px black";
 tooltip.style.width="540px"
 document.body.appendChild(tooltip);
 
-var regularText = document.createElement("div");
+regularText = document.createElement("div");
 regularText.innerHTML = "Astmete sisestamiseks tuleb trükkida ^ sümbol (enamasti SHIFT+6 klahvi kombinatsioon klaviatuuril). Et astmest väljuda, vajutage parema noole klahvi.<br><br>Vastus peab olema sisestatud normaalkujul!<br><br>Kui üksliikme arvkordaja on 1 või -1, siis tuleb see kindlasti välja kirjutada! Ehk kui õige vastus on näiteks 5x-1y+1, siis vastust<br>5x-y+1 loetakse valeks (mis sest, et sisuliselt on see samuti õige).";
 regularText.style.fontFamily="Computer Modern";
 regularText.style.fontSize="20px";
 tooltip.appendChild(regularText);
 
 KaTeX_EQ=''
-var katexEquation = document.createElement("div");
+katexEquation = document.createElement("div");
 tooltip.appendChild(katexEquation);
 
 
 // Info nuppu funktsionaalsus
-var infoNupp = document.createElement("button");
+infoNupp = document.createElement("button");
 infoNupp.innerHTML = "i";
 infoNupp.style.position = "absolute";
 infoNupp.style.margin="20px";
 infoNupp.style.padding="5px 12px";
+infoNupp.style.left = "5px";
+infoNupp.style.top = "12px";
 infoNupp.style.fontSize="20px";
 infoNupp.style.fontWeight="bold";
 infoNupp.style.fontFamily="Hoefler Text";
@@ -90,6 +93,8 @@ infoNupp.addEventListener("mouseleave", function() {
   infoNupp.style.background="transparent"
 });
 
+  
+}
 // ----------------------------------------- HTML ToolTip -------------------------------------------
 
 
@@ -184,6 +189,9 @@ function Reset(){
   Ylesanne();
   tulemus.html("");
 
+  answerMathField.focus();
+  answerMathField.latex("");
+  
   KONTROLL_NUPP=createButton("Kontroll");
   KONTROLL_NUPP.style('padding','10px 20px');
   KONTROLL_NUPP.style('background-color','MidNightBlue');
