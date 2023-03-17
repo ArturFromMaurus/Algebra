d6bd7b4db0ecae2b7eee39cacdd3850ed7781755
ArturFromMaurus <97187315+ArturFromMaurus@users.noreply.github.com>
2023-03-17 03:55:15 +0200

    window.onload

--- murdude_omadused/murd_kui_jagamis_tehe/murd_kui_jagamis_tehe.js
+++ murdude_omadused/murd_kui_jagamis_tehe/murd_kui_jagamis_tehe.js
@@ -5,11 +5,14 @@ var oige_vastus=0;
 var lopetamise_tingimus=false;
 
 
+
+window.onload = function() {
 // ----------------------------------------- HTML ToolTip -------------------------------------------
 
-var tooltip = document.createElement("div");
-tooltip.style.backgroundColor = "rgba(255, 255, 255, 255)";
-tooltip.style.color = "black";
+ tooltip = document.createElement("div");
+tooltip.style.backgroundColor = "rgba(9,9,96,0.95)"
+tooltip.style.color = "white";
+tooltip.style.borderRadius="25px";
 tooltip.style.padding = "10px";
 tooltip.style.position = "absolute";
 tooltip.style.display = "none";
@@ -18,19 +21,19 @@ tooltip.style.border="solid 2px black";
 tooltip.style.width="540px"
 document.body.appendChild(tooltip);
 
-var regularText = document.createElement("div");
+regularText = document.createElement("div");
 regularText.innerHTML = "Murru väärtuse arvutamiseks tuleb jagada lugeja (ülemine arv) nimetajaga (alumine arv). Tuhandikeni ümardamiseks tuleb ümardada lõppvastus 3 kohta pärast koma.<br><br>";
 regularText.style.fontFamily="Computer Modern";
 regularText.style.fontSize="20px";
 tooltip.appendChild(regularText);
 
 KaTeX_EQ="\\text{Näiteks: } \\dfrac{52}{12}=4.333."
-var katexEquation = document.createElement("div");
+ katexEquation = document.createElement("div");
 tooltip.appendChild(katexEquation);
 
 
 // Info nuppu funktsionaalsus
-var infoNupp = document.createElement("button");
+ infoNupp = document.createElement("button");
 infoNupp.innerHTML = "i";
 infoNupp.style.position = "absolute";
 infoNupp.style.margin="20px";
@@ -58,12 +61,7 @@ infoNupp.addEventListener("mouseleave", function() {
 });
 
 // ----------------------------------------- HTML ToolTip -------------------------------------------
-
-
-
-
-
-
+}
 
 
 function windowResized() {
